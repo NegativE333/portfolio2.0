8b1536e6d85ec19b4dfaae93295d4e00d3a35ea5
Om Tekade <omtekade914@gmail.com>
2023-05-05 16:44:17 +0530

    error fixed

--- app/skills/Skills.tsx
+++ app/skills/Skills.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import Heading from "../components/Heading";
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from "react";
 
 const Skills = () => {
+    const [ref, inView] = useInView();
+
     return(
-        <div className="lg:h-[90vh] mt-8 lg:mt-0">
+        <div 
+            ref={ref}
+            className="lg:h-[90vh] mt-8 lg:mt-0">
             <Heading 
                 title="Skills"
                 subTitle="My technical skills and expertise include"
+                inView={inView}
             />
-
         </div>
     )
 }
