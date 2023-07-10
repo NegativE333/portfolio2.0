5c075e82aed0df16d43da01226b06878d24977be
Om Tekade <omtekade914@gmail.com>
2023-07-10 23:16:06 +0530

    nav updated

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import Container from "../Container";
-import { FiPhoneCall } from 'react-icons/fi';
 import { AiOutlineMail } from 'react-icons/ai';
-import AnimatedText from "../animation/AnimatedText";
+
 
 const Navbar = () => {
+    const router = useRouter();
     return(
         <div className="fixed w-full h-[7vh] lg:h-[8.5vh] bg-zinc-800 text-white z-20 shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
                     <div className="flex flex-row items-center gap-3 md:gap-0 font-semibold ">
-                        <div className="flex-1 text-2xl lg:text-start text-center">
+                        <div 
+                            onClick={() => router.push('/')}
+                            className="flex-1 text-2xl lg:text-start text-center cursor-pointer">
                             {/* <AnimatedText text="&#60; omTekade /&#62;"/> */}
                             &#60; omTekade /&#62;
                         </div>
