0a2a9d73ee8d3e13598067ae85aa2a91ba6ad296
Om Tekade <102456428+NegativE333@users.noreply.github.com>
2023-12-01 23:04:20 +0530

    Update page.tsx

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -13,7 +13,7 @@ const ViewCV = () => {
         <>
         {domLoaded && (
             <div className="h-[100vh]">
-                <iframe className="pt-14 w-[100%] h-[100%]" src="https://drive.google.com/file/d/1XWeqlzzET6EZ7pXxUnaAzvv_-4TKJOhD/preview"></iframe>
+                <iframe className="pt-14 w-[100%] h-[100%]" src="https://drive.google.com/file/d/1LegDBffikkK2izzGyIg2g01xQqiJPsA5/view?usp=sharing"></iframe>
             </div>
         )} 
         </>
